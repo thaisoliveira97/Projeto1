427ded859c16a6820f9a8e4bcd171dfc9216271d
Thais Oliveira <thais.meninadeouro@yahoo.com.br>
2021-10-22 15:52:15 -0300

    First Controller

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC - Thais";
+            ViewData["desenvolvedora"] = "Thais de Oliveira";
 
             return View();
         }
